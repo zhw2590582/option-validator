91b0fd9500f332a5aabde0334417ee34ec883dfd
Harvey <laozhaochaguan@gmail.com>
2019-09-02 09:30:02 +0800

    1.1.4

--- __test__/shallow-object-with-validator.test.js
+++ __test__/shallow-object-with-validator.test.js
@@ -1,66 +1,100 @@
 import optionValidator from '../index';
 
-test('Shallow object with scheme validator', () => {
-  optionValidator({
-    typeNumber: 42,
-    typeString: 'str',
-    typeObject: {},
-    typeArray: [1, 2, 3]
-  }, {
-    typeNumber: {
-      type: 'number',
-      validator: (value, type, paths) => {
-        return value === 42;
-      }
-    },
-    typeString: {
-      type: 'string',
-      validator: (value, type, paths) => {
-        return value.length === 3;
-      }
-    },
-    typeObject: {
-      type: 'object',
-      validator: (value, type, paths) => {
-        return Object.keys(value).length === 0;
-      }
-    },
-    typeArray: {
-      type: 'array',
-      validator: (value, type, paths) => {
-        return value.length === 3;
-      }
-    }
-  });
+test('Shallow object with scheme validator 1', () => {
+    optionValidator(
+        {
+            typeNumber: 42,
+            typeString: 'str',
+            typeObject: {},
+            typeArray: [1, 2, 3],
+        },
+        {
+            typeNumber: {
+                type: 'number',
+                validator: (value, type, paths) => {
+                    return value === 42;
+                },
+            },
+            typeString: {
+                type: 'string',
+                validator: (value, type, paths) => {
+                    return value.length === 3;
+                },
+            },
+            typeObject: {
+                type: 'object',
+                validator: (value, type, paths) => {
+                    return Object.keys(value).length === 0;
+                },
+            },
+            typeArray: {
+                type: 'array',
+                validator: (value, type, paths) => {
+                    return value.length === 3;
+                },
+            },
+        },
+    );
+});
+
+test('Shallow object with scheme validator 2', () => {
+    optionValidator(
+        {
+            typeNumber: 42,
+            typeString: 'str',
+            typeObject: {},
+            typeArray: [1, 2, 3],
+        },
+        {
+            typeNumber: (value, type, paths) => {
+                return value === 42;
+            },
+            typeString: (value, type, paths) => {
+                return value.length === 3;
+            },
+            typeObject: (value, type, paths) => {
+                return Object.keys(value).length === 0;
+            },
+            typeArray: (value, type, paths) => {
+                return value.length === 3;
+            },
+        },
+    );
 });
 
 test('Shallow object with scheme validator and option validator', () => {
-  optionValidator({
-    validator: 42,
-    __validator__: 'str'
-  }, {
-    validator: {
-      validator: (value, type, paths) => {
-        return value === 42;
-      }
-    },
-    __validator__: {
-      validator: (value, type, paths) => {
-        return value.length === 3;
-      }
-    }
-  });
+    optionValidator(
+        {
+            validator: 42,
+            __validator__: 'str',
+        },
+        {
+            validator: {
+                validator: (value, type, paths) => {
+                    return value === 42;
+                },
+            },
+            __validator__: {
+                validator: (value, type, paths) => {
+                    return value.length === 3;
+                },
+            },
+        },
+    );
 });
 
 test('Shallow object with scheme validator and option without validator', () => {
-  optionValidator({
-    typeNumber: 42,
-    typeString: 'str',
-    typeObject: {},
-    typeArray: [1, 2, 3]
-  }, {
-    validator: (value, type, paths) => {
-      return Object.keys(value).length === 4;
-    }
-  });
+    optionValidator(
+        {
+            typeNumber: 42,
+            typeString: 'str',
+            typeObject: {},
+            typeArray: [1, 2, 3],
+        },
+        {
+            validator: (value, type, paths) => {
+                return Object.keys(value).length === 4;
+            },
+        },
+    );
 });
--- index.js
+++ index.js
@@ -1,97 +1,101 @@
 import kindOf from 'kind-of';
 
 function optionValidator(option, scheme, paths = ['option']) {
-  checkType(option, scheme, paths);
-  checkValidator(option, scheme, paths);
-  checkChild(option, scheme, paths);
+    checkType(option, scheme, paths);
+    checkValidator(option, scheme, paths);
+    checkChild(option, scheme, paths);
 
-  for (const key in scheme) {
-    if (Object.prototype.hasOwnProperty.call(scheme, key)) {
-      const optionValue = option[key];
-      const schemeValue = scheme[key];
-      const currentPath = paths.concat(key);
-      if (checkRequired(option, key, schemeValue, currentPath)) continue;
-      checkType(optionValue, schemeValue, currentPath);
-      checkValidator(optionValue, schemeValue, currentPath);
-      checkChild(optionValue, schemeValue, currentPath);
+    for (const key in scheme) {
+        if (Object.prototype.hasOwnProperty.call(scheme, key)) {
+            const optionValue = option[key];
+            const schemeValue = scheme[key];
+            const currentPath = paths.concat(key);
+            if (checkRequired(option, key, schemeValue, currentPath)) continue;
+            checkType(optionValue, schemeValue, currentPath);
+            checkValidator(optionValue, schemeValue, currentPath);
+            checkChild(optionValue, schemeValue, currentPath);
+        }
     }
-  }
 
-  return option;
+    return option;
 }
 
 function checkRequired(option, key, schemeValue, paths) {
-  if (!Object.prototype.hasOwnProperty.call(option, key)) {
-    if (schemeValue.__required__ === true || schemeValue.required === true) {
-      throw new TypeError(`'${paths.join('.')}' is required`);
-    } else {
-      return true;
+    if (!Object.prototype.hasOwnProperty.call(option, key)) {
+        if (schemeValue.__required__ === true || schemeValue.required === true) {
+            throw new TypeError(`'${paths.join('.')}' is required`);
+        } else {
+            return true;
+        }
     }
-  }
 }
 
 function checkType(optionValue, schemeValue, paths) {
-  let schemeType;
-  if (kindOf(schemeValue) === 'string') {
-    schemeType = schemeValue;
-  } else if (kindOf(schemeValue) === 'function') {
-    schemeValue.___validator__ = schemeValue;
-  } else if (schemeValue.__type__) {
-    schemeType = schemeValue.__type__;
-  } else if (schemeValue.type) {
-    schemeType = schemeValue.type;
-  }
-
-  if (schemeType && kindOf(schemeType) === 'string') {
-    schemeType = schemeType.trim().toLowerCase();
-    const optionType = kindOf(optionValue);
-    let result = optionType === schemeType;
-    if (schemeType.indexOf('|') > -1) {
-      const schemeTypes = schemeType.split('|');
-      result = schemeTypes.filter(Boolean).some(item => optionType === item.trim());
+    let schemeType;
+    if (kindOf(schemeValue) === 'string') {
+        schemeType = schemeValue;
+    } else if (kindOf(schemeValue) === 'function') {
+        schemeValue.___validator__ = schemeValue;
+    } else if (schemeValue.__type__) {
+        schemeType = schemeValue.__type__;
+    } else if (schemeValue.type) {
+        schemeType = schemeValue.type;
     }
 
-    if (!result) {
-      throw new TypeError(`'${paths.join('.')}' require '${schemeType}' type, but got '${optionType}'`);
+    if (schemeType && kindOf(schemeType) === 'string') {
+        schemeType = schemeType.trim().toLowerCase();
+        const optionType = kindOf(optionValue);
+        let result = optionType === schemeType;
+        if (schemeType.indexOf('|') > -1) {
+            const schemeTypes = schemeType.split('|');
+            result = schemeTypes.filter(Boolean).some(item => optionType === item.trim());
+        }
+
+        if (!result) {
+            throw new TypeError(`'${paths.join('.')}' require '${schemeType}' type, but got '${optionType}'`);
+        }
     }
-  }
 }
 
 function checkValidator(optionValue, schemeValue, paths) {
-  let schemeValidator;
-  if (schemeValue.___validator__) {
-    schemeValidator = schemeValue.___validator__;
-  } else if (schemeValue.validator) {
-    schemeValidator = schemeValue.validator;
-  }
-
-  if (kindOf(schemeValidator) === 'function') {
-    const optionType = kindOf(optionValue);
-    const result = schemeValidator(optionValue, optionType, paths);
-    if (result !== true) {
-      throw new TypeError(`The scheme for '${paths.join('.')}' validator function require return true, but got '${result}'`);
+    let schemeValidator;
+    if (kindOf(schemeValue) === 'function') {
+        schemeValidator = schemeValue;
+    } else if (schemeValue.___validator__) {
+        schemeValidator = schemeValue.___validator__;
+    } else if (schemeValue.validator) {
+        schemeValidator = schemeValue.validator;
     }
-  }
+
+    if (kindOf(schemeValidator) === 'function') {
+        const optionType = kindOf(optionValue);
+        const result = schemeValidator(optionValue, optionType, paths);
+        if (result !== true) {
+            throw new TypeError(
+                `The scheme for '${paths.join('.')}' validator function require return true, but got '${result}'`,
+            );
+        }
+    }
 }
 
 function checkChild(optionValue, schemeValue, paths) {
-  let schemeChild;
-  if (schemeValue.___child__) {
-    schemeChild = schemeValue.___child__;
-  } else if (schemeValue.child) {
-    schemeChild = schemeValue.child;
-  }
-
-  if (kindOf(schemeChild) === 'object') {
-    const optionType = kindOf(optionValue);
-    if (optionType === 'object') {
-      optionValidator(optionValue, schemeChild, paths);
-    } else if (optionType === 'array') {
-      optionValue.forEach((item, index) => {
-        optionValidator(item, schemeChild, paths.concat(index));
-      });
+    let schemeChild;
+    if (schemeValue.___child__) {
+        schemeChild = schemeValue.___child__;
+    } else if (schemeValue.child) {
+        schemeChild = schemeValue.child;
     }
-  }
+
+    if (kindOf(schemeChild) === 'object') {
+        const optionType = kindOf(optionValue);
+        if (optionType === 'object') {
+            optionValidator(optionValue, schemeChild, paths);
+        } else if (optionType === 'array') {
+            optionValue.forEach((item, index) => {
+                optionValidator(item, schemeChild, paths.concat(index));
+            });
+        }
+    }
 }
 
 optionValidator.kindOf = kindOf;
